8ec519e5ffe7c4980ecff9b099ed0fc412e3e769
Владлена Бугоркова <vladabugorokmak@gmail.com>
2024-05-22 19:29:25 +0700

    создание события, оформление подписки на него

---
+++ events/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace events {
+    internal class Program {
+        public class UserEnteredNumberEventArgs {
+            public int Input { get; set; }
+            public DateTime EnteredAt { get; set; }
+        }
+
+        // Событие
+        public static event EventHandler<UserEnteredNumberEventArgs> UserEnteredNumber;
+
+        // Функция вывода числа введенного пользователем
+        private static void PrintUserEnteredNumber(object sender, UserEnteredNumberEventArgs e) {
+            Console.WriteLine($"\nПользователь ввел число {e.Input} в {e.EnteredAt}\n");
+        }
+
+        static void Main(string[] args) {
+            string input;
+            int number;
+
+            UserEnteredNumber += PrintUserEnteredNumber;
+
+            while (true) {
+                Console.Write("Введите число > ");
+
+                input = Console.ReadLine();
+
+                // Проверка что строка является числом
+                if (int.TryParse(input, out number)) {
+                    // Вызываем событие с объектом аргументов
+                    UserEnteredNumber?.Invoke(null, new UserEnteredNumberEventArgs { Input = number, EnteredAt = DateTime.Now });
+                }
+            }
+        }
+    }
+}
